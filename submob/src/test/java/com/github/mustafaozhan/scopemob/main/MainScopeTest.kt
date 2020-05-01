4622e33a5a2b26f4c66d4607c0ae3d27458175e4
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-05-01 18:43:08 +0200

    copyright update

--- submob/src/test/java/com/github/mustafaozhan/scopemob/main/MainScopeTest.kt
+++ submob/src/test/java/com/github/mustafaozhan/scopemob/main/MainScopeTest.kt
@@ -1,4 +1,6 @@
-// Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+/*
+ Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+ */
 package com.github.mustafaozhan.scopemob.main
 
 import com.github.mustafaozhan.scopemob.either
