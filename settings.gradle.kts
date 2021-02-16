3150c4609920c31d87e1f42fba2a5ebabc906641
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-02-16 15:52:55 +0100

    resolutionStrategy added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,21 @@
 /*
  Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
  */
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        jcenter()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:4.0.1")
+            }
+        }
+    }
+}
+
 rootProject.name = "scopemob"
 include(":submob")
