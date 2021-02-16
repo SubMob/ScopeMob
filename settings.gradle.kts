bdb5505aedfc2193dda1ed2ea19d786ce1291ae9
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-02-16 16:00:49 +0100

    resolutionStrategy added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,14 @@
 /*
  Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
  */
+enableFeaturePreview("GRADLE_METADATA")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         jcenter()
         mavenCentral()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:4.0.1")
-            }
-        }
     }
 }
 
