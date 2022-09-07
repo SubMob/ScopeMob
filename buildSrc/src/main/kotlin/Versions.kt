8c4f75fa23a3f7f169755ca0aec4d5e0adbe889a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-09-07 21:42:40 +0200

    [#19] Update Libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,5 +6,5 @@ object Versions {
     const val KOTLIN = "1.7.10"
     const val KOVER = "0.5.1"
     const val DEPENDENCY_UPDATES = "0.42.0"
-    const val BUILD_HEALTH = "1.10.0"
+    const val BUILD_HEALTH = "1.13.1"
 }
