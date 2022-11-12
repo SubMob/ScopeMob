aab23c9d63dca73aff4111c401246b83795834bf
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-11-12 15:05:26 +0300

    [Oztechan/CCC#1298] Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@
  */
 
 object Versions {
-    const val KOTLIN = "1.7.20"
+    const val KOTLIN = "1.7.21"
     const val KOVER = "0.6.1"
-    const val DEPENDENCY_UPDATES = "0.43.0"
+    const val DEPENDENCY_UPDATES = "0.44.0"
 }
