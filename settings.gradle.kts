e5e2277040092b235dbf3b4c8b96854dff27ad8a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-11-02 00:02:33 +0100

    [Oztechan/CCC#2943] Add gradle build scan to CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,23 @@
  Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
  */
 
+plugins {
+    id("com.gradle.enterprise") version ("3.13")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+
+        obfuscation {
+            username { null }
+            hostname { null }
+            ipAddresses { null }
+        }
+    }
+}
+
 rootProject.name = "ScopeMob"
 include(":scopemob")
