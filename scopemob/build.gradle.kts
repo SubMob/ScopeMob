bed3d01ddd94cc5d97dc1f63e254ce5c7723d81e
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-11-02 11:20:07 +0100

    [SubMob/ScopeMob#32] Dependency update kotlin monorepo to v1.9.20

--- scopemob/build.gradle.kts
+++ scopemob/build.gradle.kts
@@ -9,24 +9,16 @@ plugins {
 }
 
 kotlin {
-    @Suppress("OPT_IN_USAGE")
-    targetHierarchy.default()
-
     jvm()
 
     iosX64()
     iosArm64()
     iosSimulatorArm64()
 
-    @Suppress("UNUSED_VARIABLE")
-    sourceSets {
-        val commonTest by getting {
-            dependencies {
-                libs.common.apply {
-                    implementation(test)
-                    implementation(testAnnotations)
-                }
-            }
+    sourceSets.commonTest.dependencies {
+        libs.common.apply {
+            implementation(test)
+            implementation(testAnnotations)
         }
     }
 }
