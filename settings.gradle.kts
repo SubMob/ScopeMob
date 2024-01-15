c87536c9ed31ecc6c43707d1704f4571b6859518
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-01-15 17:03:27 +0200

    [Oztechan/CCC#3078] Update build.gradle files with conventional names for easy navigation for submodules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,20 @@ gradleEnterprise {
     }
 }
 
-rootProject.name = "ScopeMob"
 include(":scopemob")
+
+rootProject.name = "ScopeMob"
+rootProject.updateBuildFileNames()
+
+fun ProjectDescriptor.updateBuildFileNames() {
+    buildFileName = path
+        .drop(1)
+        .replace(":", "-")
+        .dropLastWhile { it != '-' }
+        .plus(name)
+        .plus(".gradle.kts")
+
+    if (children.isNotEmpty()) {
+        children.forEach { it.updateBuildFileNames() }
+    }
+}
