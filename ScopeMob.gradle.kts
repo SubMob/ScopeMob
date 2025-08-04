ea075de24c9acae4c2008243783f6058f84189c9
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-08-04 19:07:45 +0200

    [Oztechan/CCC#4392] Remove combined dependency task

--- ScopeMob.gradle.kts
+++ ScopeMob.gradle.kts
@@ -96,12 +96,6 @@ allprojects {
     }
 }
 
-tasks.findByName("dependencies")?.let {
-    allprojects.forEach { prj ->
-        if (prj != rootProject) it.dependsOn("${prj.path}:dependencies")
-    }
-}
-
 val isReleaseBuild: Boolean
     get() = System.getenv("GPG_KEY") != null
 
